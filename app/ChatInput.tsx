9c2608c58582f9c0a8f9eb83c4d574df1ea33e09
Mauricio <mauricio.cds00@gmail.com>
2023-01-20 09:56:18 -0300

    feat: chat inpuit done and pre redis connection

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 const ChatInput = () => {
   const [message, setMessage] = useState("");
 
+  const handleMessage = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!message) return;
+
+    const messageToSend = message;
+
+    setMessage("");
+
+    console.log(messageToSend);
+  };
+
   return (
-    <form className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100">
+    <form
+      onSubmit={handleMessage}
+      className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100"
+    >
       <input
         type="text"
         value={message}
